0ea3abe197e49872f692f83dea444abfd4090b32
Jakob Lykke Andersen <Jakob@caput.dk>
2025-04-15 22:24:44 +0200

    Add open-babel-fixed

---
+++ Formula/open-babel-fixed.rb
@@ -0,0 +1,63 @@
+class OpenBabelFixed < Formula
+  desc "Chemical toolbox"
+  homepage "https://github.com/openbabel/openbabel"
+  license "GPL-2.0-only"
+  revision 1
+  head "https://github.com/jakobandersen/openbabel.git", branch: "fixed"
+
+  depends_on "cmake" => :build
+  depends_on "pkgconf" => :build
+  depends_on "rapidjson" => :build
+  depends_on "swig" => :build
+
+  depends_on "cairo"
+  depends_on "eigen"
+  depends_on "inchi"
+  depends_on "python@3.13"
+
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  def python3
+    "python3.13"
+  end
+
+  conflicts_with "surelog", because: "both install `roundtrip` binaries"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DINCHI_INCLUDE_DIR=#{Formula["inchi"].opt_include}/inchi",
+                    "-DOPENBABEL_USE_SYSTEM_INCHI=ON",
+                    "-DRUN_SWIG=ON",
+                    "-DPYTHON_BINDINGS=ON",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    "-DPYTHON_INSTDIR=#{prefix/Language::Python.site_packages(python3)}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_match <<~EOS, shell_output("#{bin}/obabel -:'C1=CC=CC=C1Br' -omol")
+
+        7  7  0  0  0  0  0  0  0  0999 V2000
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+          0.0000    0.0000    0.0000 Br  0  0  0  0  0  0  0  0  0  0  0  0
+        1  6  1  0  0  0  0
+        1  2  2  0  0  0  0
+        2  3  1  0  0  0  0
+        3  4  2  0  0  0  0
+        4  5  1  0  0  0  0
+        5  6  2  0  0  0  0
+        6  7  1  0  0  0  0
+      M  END
+    EOS
+
+    system python3, "-c", "from openbabel import openbabel"
+  end
+end
